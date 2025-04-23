b8d89c859ae5f88c6959971e371d5137e264f91f
Nicolò Ermanno Millo <ermannomillo@outlook.it>
2025-04-23 23:45:11 +0200

    First RC and motors calibration.

--- Quadcopter_UniTS/Core/Inc/motor.h
+++ Quadcopter_UniTS/Core/Inc/motor.h
@@ -3,10 +3,10 @@
 
 #include "stm32h7xx_hal.h"
 
-#define MOTOR_MAX_PWM_VALUE     1700.0f  
-#define MOTOR_MIN_PWM_VALUE     850.0f     
+#define MOTOR_MAX_PWM_VALUE     600
+#define MOTOR_MIN_PWM_VALUE     0
 
-void set_motor_pwm(float motor_pwm[]);
-void set_motor_pwm_zero(float motor_pwm[]);
+void set_motor_pwm(uint32_t motor_pwm[]);
+void set_motor_pwm_zero(uint32_t motor_pwm[]);
 
 #endif 
--- Quadcopter_UniTS/Core/Inc/radio.h
+++ Quadcopter_UniTS/Core/Inc/radio.h
@@ -2,19 +2,19 @@
 #define _RC_H_
 
 
-#define RC_FULLSCALE        1800
+#define RC_FULLSCALE        1000
 
 
 #define PI  3.141592654f
 
 #define PITCH_MAX_DEG   30
 #define ROLL_MAX_DEG    30
+#define SENSOR_SAMPLING_TIME    0.02 // 50 Hz RC frequency
 
-//#define YAW_MAX_DEG     (180.0*SENSOR_SAMPLING_TIME)
-#define YAW_MAX_DEG     2.0f //(60.0f*SENSOR_SAMPLING_TIME)
+#define YAW_MAX_DEG     (60.0f * SENSOR_SAMPLING_TIME)
 #define YAW_MIN_RAD     0.0872
 
-#define EULER_Z_TH      600
+#define YAW_DEAD_THR    300
 
 void get_target_euler(float euler_rc[], float rc_comm[]);
 
